44d4acd760c29ed38a32a55065bc38eeec9cf83a
KhushBakht334 <44219882+KhushBakht334@users.noreply.github.com>
2023-04-19 17:22:31 +0500

    buttons

--- src/components/News.js
+++ src/components/News.js
@@ -2,40 +2,40 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  articles=[
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-]
 constructor(){
 super();
 this.state = {
-  articles: this.articles
+  articles: [],
+  loading:false,
+  page:1
 }
 }
 async componentDidMount(){
-  let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=00d639c2e9cf4c8cb043d42cb9c32c30`;
+  let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=00d639c2e9cf4c8cb043d42cb9c32c30&page=1&pageSize=20`;
   let data=await fetch(url);
   let parseData=await data.json()
   console.log(parseData)
-  this.setState({articles: parseData.articles})
+  this.setState({articles: parseData.articles,
+    totalResults: parseData.totalResults})
+}
+handleNextClick= async()=>{
+  if( this.state.page +1 > Math.ceil(this.state.totalResults/20))
+  {}else{
+  let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=00d639c2e9cf4c8cb043d42cb9c32c30&page=${this.state.page +1}&pageSize=20`;
+  let data=await fetch(url);
+  let parseData=await data.json()
+  console.log(parseData)
+  this.setState({articles: parseData.articles,
+    page: this.state.page +1})
+  }
+}
+handlePrevClick= async()=>{
+  let url=`https://newsapi.org/v2/top-headlines?country=us&apiKey=00d639c2e9cf4c8cb043d42cb9c32c30&page=${this.state.page - 1}&pageSize=20`;
+  let data=await fetch(url);
+  let parseData=await data.json()
+  console.log(parseData)
+  this.setState({articles: parseData.articles,
+    page: this.state.page - 1})
 }
   render() {
     return (
@@ -46,6 +46,10 @@ async componentDidMount(){
           return <div className='col-md-4' key={element.url}>
             <NewsItem title={element.title?element.title.slice(0,20):""} description={element.description?element.description.slice(0,45):""} imageUrl={element.urlToImage?element.urlToImage:"https://ichef.bbci.co.uk/news/1024/branded_news/C705/production/_129394905_front.jpg"} newsUrl={element.url} />
             </div>})}
+        </div>
+        <div className='container d-flex justify-content-between'>
+        <button disabled={this.state.page <=1 }type="button" className="btn btn-outline-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" className="btn btn-outline-dark" onClick={this.handleNextClick}>  &rarr; Next</button>
         </div>
       </div>
      
